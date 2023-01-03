39fc4ed8ae7b847f31dc22475a5876c70b54f6d7
skyv26 <dev.aakashv@gmail.com>
2023-01-03 18:39:20 +0530

    Update `Food` model table

--- db/migrate/20230103124305_create_foods.rb
+++ db/migrate/20230103124305_create_foods.rb
@@ -5,6 +5,7 @@ class CreateFoods < ActiveRecord::Migration[7.0]
       t.string :measurement_unit
       t.integer :price
       t.integer :quantity, limit: 2
+      t.integer :recipe_food_counter, default: 0
       t.references :owner, references: :users, null: false, foreign_key: {to_table: :users}
 
       t.timestamps
