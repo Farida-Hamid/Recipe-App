8efd78d07e584a3f0d312ee9ecbb0bdfb8eddadd
Farida-Hamid <faridaaddala@gmail.com>
2023-01-05 14:56:07 +0300

    Adjust controller to add a Recipe

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,2 +1,26 @@
 class RecipeController < ApplicationController
+  def index
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    @recipe.owner_id = User.first.id
+
+    if @recipe.save
+      redirect_to recipe_index_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:new_recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
