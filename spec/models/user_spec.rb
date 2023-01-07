972dd5b595eccd87177c21983c7fd7328b53bb99
skyv26 <dev.aakashv@gmail.com>
2023-01-07 11:17:45 +0530

    Add the rspec test for `user-model`

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Farida', email: 'farida@gmail.com', password: 'topsecret', password_confirmation: 'topsecret', role: 'admin') }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should be "Farida"' do
+    expect(subject.name).to eql("Farida")
+  end
+
+  it 'email should be present' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should be "farida@gmail.com"' do
+    expect(subject.email).to eql("farida@gmail.com")
+  end
+
+  it 'role should be "admin"' do
+    expect(subject.role).to eql('admin')
+  end
 end
